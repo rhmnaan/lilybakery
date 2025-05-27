64e645c76312a6bd8c548efc1da8907fa2503205
HENGKIDWI <hengki.dwi.kurniawan.05@gmail.com>
2025-05-27 14:08:54 +0700

    Resolve merge conflict in web.php

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,6 @@ Route::get('/about-us', function () {
     return view('about-us');
 });
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 23eb2fc99bc707a55f2da3544cca01a9f0c3831a
 Route::get('/promotion', function () {
     return view('promotion');
 });
@@ -22,29 +18,18 @@ Route::get('/stores', function () {
     return view('stores');
 });
 
-<<<<<<< HEAD
-Route::get('/privacy-policy', function () {
-    return view('privacy-policy');
-=======
 Route::get('/product-detail', function () {
     return view('product-detail');
 });
 
 Route::get('/payment-method', function () {
     return view('payment-method');
->>>>>>> 23eb2fc99bc707a55f2da3544cca01a9f0c3831a
 });
 
 Route::get('/delivery-info', function () {
     return view('delivery-info');
 });
 
-<<<<<<< HEAD
-Route::get('/payment-method', function () {
-    return view('payment-method');
-});
-
-=======
 Route::get('/privacy-policy', function () {
     return view('privacy-policy');
 });
@@ -54,16 +39,12 @@ Route::get('/contact', function () {
 });
 
 
->>>>>>> 23eb2fc99bc707a55f2da3544cca01a9f0c3831a
 Route::get('/login', function () {
     return view('login');
 });
 
 Route::get('/register', function () {
     return view('register');
-<<<<<<< HEAD
-});
-=======
 });
 
 Route::get('/admin-login', function () {
@@ -84,4 +65,3 @@ Route::get('Admin/admin-dashboard', function () {
     return view('Admin/admin-dashboard');
 });
 
->>>>>>> 23eb2fc99bc707a55f2da3544cca01a9f0c3831a
